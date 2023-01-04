5c447993761dbf439d9c385ad75028a44f9b52a4
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-04 23:21:06 +0100

    Branch isShown

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
 import "./App.css";
-
-const window = document.getElementById("window");
-console.log(window);
-window.style.display = "none";
-
-function Window() {
-  return (
-    <>
-      <div id="window"></div>
-    </>
-  );
-}
-
-function Div() {
-  return (
-    <>
-      <div className="divStyle">
-        <button
-          id="button"
-          onClick={function () {
-            window.style.display = "block";
-          }}
-        >
-          DETAILS
-        </button>
-      </div>
-    </>
-  );
-}
+import { useState } from "react";
 
 function App() {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = (event) => {
+    // 👇️ toggle shown state
+    setIsShown(true);
+  };
+
+  const handleClickClose = (event) => {
+    // 👇️ toggle shown state
+    setIsShown(false);
+  };
+
   return (
     <>
       <h1>Products</h1>
       <div id="main">
-        <Div />
-        <Window />
+        <div className="divStyle">
+          <button id="button" onClick={handleClick}>
+            DETAILS
+          </button>
+        </div>
+        <Window onClick={handleClickClose} />
       </div>
     </>
   );
+}
+
+function Window(isShown, onClick) {
+  return <>{isShown && <div id="window" onClick={onClick}></div>}</>;
 }
 
 export default App;
