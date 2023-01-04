d2797a856aed10ec2713af4db048a4ef0f7f2e45
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-04 13:28:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+
+const window = document.getElementById("window");
+console.log(window);
+window.style.display = "none";
+
+function Window() {
+  return (
+    <>
+      <div id="window"></div>
+    </>
+  );
+}
+
+function Div() {
+  return (
+    <>
+      <div className="divStyle">
+        <button id="button">DETAILS</button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Products</h1>
+      <div id="main">
+        <Div />
+        <Window />
+      </div>
+    </>
+  );
+}
+
+export default App;
